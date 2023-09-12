058d322e5e8d6492e72c6e8f3c7f2d9322ce3c38
susant patel <susant.patel01@gmail.com.com>
2023-09-12 09:42:55 +0530

    internal server error

---
+++ pages/api/post/index.ts
@@ -0,0 +1,25 @@
+import { getSession } from "next-auth/react";
+import prisma from "../../../lib/prisma";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  const session = await getSession({ req });
+  console.log({ session });
+  if (!session) {
+    // Handle the case when the user is not authenticated
+    return res.status(401).end();
+  }
+
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
